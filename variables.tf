43ee5d9d3d988c3ffc1f2638efe27e113396e143
pradeepkumar-27 <pradeepkumarr.2k@gmail.com>
2022-10-12 15:11:12 +0530

    DEV-10004: terraform template repository

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "resource_owner" {
+  description = "resource owner"
+  type        = string
+}
